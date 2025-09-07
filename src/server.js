4ff59156cc03e5a497cac8ac86fd563494f21541
Alis Patel <alispatel123098@gmail.com>
2025-09-07 23:40:17 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -29,6 +29,8 @@ app.use(
           ]
         : ["http://localhost:5173", "http://localhost:3000"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   }),
 )
 
